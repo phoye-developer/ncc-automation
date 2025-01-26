cdb5d4f22290a97e604eca33fb06f3a15d7376a9
Patrick Hoye <phoye.developer@gmail.com>
2025-01-25 21:37:24 -0700

    Add update to chat survey upon campaign creation

--- ncc_survey.py
+++ ncc_survey.py
@@ -3,6 +3,22 @@ import urllib.parse
 import json
 
 
+def get_survey(ncc_location: str, ncc_token: str, survey_id: str) -> list:
+    """
+    This function fetches the details of a survey present on the Nextiva Contact Center (NCC) tenant.
+    """
+    survey = {}
+    conn = http.client.HTTPSConnection(ncc_location)
+    payload = ""
+    headers = {"Authorization": ncc_token}
+    conn.request("GET", f"/data/api/types/survey/{survey_id}", payload, headers)
+    res = conn.getresponse()
+    if res.status == 200:
+        data = res.read().decode("utf-8")
+        survey = json.loads(data)
+    return survey
+
+
 def get_surveys(ncc_location: str, ncc_token: str) -> list:
     """
     This function fetches a list of surveys present on the Nextiva Contact Center tenant.
@@ -88,6 +104,39 @@ def create_survey(
         survey = json.loads(data)
     conn.close()
     return survey
+
+
+def update_chat_survey_campaign_id(
+    ncc_location: str, ncc_token: str, survey_id: str, campaign_id: str
+) -> bool:
+    """
+    This function updates the campaign ID associated with two buttons on the chat survey.
+    """
+    success = False
+    survey = get_survey(ncc_location, ncc_token, survey_id)
+    if survey != {}:
+        conn = http.client.HTTPSConnection(ncc_location)
+        survey["layout"]["elements"][1]["elements"][0]["elements"][0]["elements"][6][
+            "elements"
+        ][0]["properties"]["campaignId"] = campaign_id
+        survey["layout"]["elements"][1]["elements"][0]["elements"][0]["elements"][6][
+            "elements"
+        ][2]["properties"]["campaignId"] = campaign_id
+        payload = json.dumps(survey)
+        headers = {
+            "Authorization": ncc_token,
+            "Content-Type": "application/json",
+        }
+        conn.request(
+            "PATCH",
+            f"/data/api/types/survey/{survey_id}",
+            payload,
+            headers,
+        )
+        res = conn.getresponse()
+        if res.status == 200:
+            success = True
+    return success
 
 
 def delete_survey(ncc_location: str, ncc_token: str, survey_id: str) -> bool:
