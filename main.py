8329c2ec06c45dd9cd0e4a0e331f048b72c51d86
Patrick Hoye <phoye.developer@gmail.com>
2025-03-10 14:28:56 -0700

    Update version number in login banner

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
     print("See www.nextiva.com for details.")
     print("*****************************************************")
     print()
-    print("NCC Automation v1.1.0")
+    print("NCC Automation v2.0.0-beta")
     print()
     print("Please log in. Your password will not display on the screen.")
     print()
